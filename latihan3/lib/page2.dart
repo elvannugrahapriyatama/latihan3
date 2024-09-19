b78ab8f6d821cd76ae749d810af037359af89621
elvannugrahapriyatama <elvanmarhas@gmail.com>
2024-09-19 09:49:50 +0700

    Update page2.dart

--- latihan3/lib/page2.dart
+++ latihan3/lib/page2.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class Page2 extends StatelessWidget {
   const Page2({super.key});
@@ -7,8 +8,39 @@ class Page2 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('ini halaman 2'),
-      ),
+          child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.red,
+              ),
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.green,
+              )
+            ],
+          ),
+          Row(
+            children: [
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.blue,
+              ),
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.amber,
+              )
+            ],
+          )
+        ],
+      )),
     );
   }
 }
